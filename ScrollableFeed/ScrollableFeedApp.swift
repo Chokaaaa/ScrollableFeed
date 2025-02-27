49aa9649317c4987db40d3b614a972ae1d985398
Nursultan <nursultanyelemessov@gmail.com>
2025-02-27 15:33:18 +0400

    Initial Commit

---
+++ ScrollableFeed/ScrollableFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableFeedApp.swift
+//  ScrollableFeed
+//
+//  Created by Nursultan Yelemessov on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
